99860844051ed35e95484004e15d2a247829f20b
Stanislav Li <sli@nurtelecom.kg>
2024-06-07 13:28:53 +0600

    Added realm dependency

--- Package.swift
+++ Package.swift
@@ -14,10 +14,14 @@ let package = Package(
             type: .dynamic,
             targets: ["WrapKit"])
     ],
+    dependencies: [.package(url: "https://github.com/realm/realm-swift.git", exact: "10.44.0")],
     targets: [
         .target(
             name: "WrapKit",
-            dependencies: [],
+            dependencies: [
+                .product(name: "Realm", package: "realm-swift"),
+                .product(name: "RealmSwift", package: "realm-swift")
+            ],
             path: "WrapKitCore/Sources"
         ),
         .testTarget(
